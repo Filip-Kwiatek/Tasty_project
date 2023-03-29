9b971d4c408f2c6e520ac06e8b1018c23bb4d3fa
Filip <filip.kwiatek.pl@gmail.com>
2023-03-29 21:35:57 +0200

    Adding Shopping Section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar.js";
 import SearchBar from "./components/SearchBar/SearchBar";
 import Wrapper from "./components/UI/Wrapper";
 import Slider from "./components/Slider/Slider";
+import ShopifySection from "./components/ShopifySection/ShopifySection";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <SearchBar />
       <Navbar />
       <Slider />
+      <ShopifySection/>
     </Wrapper>
   );
 }
---
+++ src/components/ShopifySection/ShopifySection.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./ShopifySection.module.css";
+
+const ShopifySection = () => {
+  return <div className={classes.shopping_frame}>Rest content</div>;
+};
+
+export default ShopifySection;
